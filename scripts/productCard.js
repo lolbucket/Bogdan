c7745836461c2902052117bddd69aff5ca586b0a
Bogdan <bgryshan@gmail.com>
2023-01-04 13:12:29 +0200

    feat: add path to product page

--- scripts/productCard.js
+++ scripts/productCard.js
@@ -1,13 +1,18 @@
-export const createProductCardHTML = ({ title, price, thumbnail }) => `
+const PRODUCT_PATH = './pages/products/iphone-13.html';
+
+export const createProductCardHTML = (
+  { title, price, thumbnail },
+  path = PRODUCT_PATH,
+) => `
 	<article class="product-card">
 		<a
-			href="#"
+			href="${path}"
 			class="product-card__img-wrapper"
 		>
 			<img src="${thumbnail}" alt="${title}" />
 		</a>
 		<div class="product-card__content">
-			<a href="#" class="product-card__title">${title}</a>
+			<a href="${path}" class="product-card__title">${title}</a>
 			<span class="product-card__price">${price}$</span>
 		</div>
 	</article>
