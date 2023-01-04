0ce7d06aed781e9f8ee2273849003984e7d9a23d
Bogdan <bgryshan@gmail.com>
2023-01-04 12:51:35 +0200

    feat: return price filter values

--- scripts/cataloguePage.js
+++ scripts/cataloguePage.js
@@ -1,3 +1,25 @@
 import { displayProducts } from './products.js';
 
 displayProducts();
+
+const getPriceFilterValues = () => {
+  const categoryPriceForm = document.querySelector('.categories__price');
+
+  categoryPriceForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const minField = e.currentTarget.children[0];
+    const maxField = e.currentTarget.children[2];
+
+    const priceFilterValues = {
+      min: minField.value,
+      max: maxField.value,
+    };
+
+    console.log(priceFilterValues);
+
+    return priceFilterValues;
+  });
+};
+
+getPriceFilterValues();
